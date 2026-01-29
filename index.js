443020aac98681c6886aa0216a75a832cbe0dce4
Adel Ahmed <adel15952@gmail.com>
2026-01-30 00:59:03 +0200

    feat: implement internationalization with i18next and middleware.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const i18next = require("i18next");
+const middleware = require("i18next-http-middleware");
+const Backend = require("i18next-fs-backend");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 const api = process.env.API_PREFIX;
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    backend: { loadPath: "locales/{{lng}}.json" },
+  });
+
+app.use(middleware.handle(i18next));
 
 mongoose
   .connect(process.env.MONGODB_URI)
