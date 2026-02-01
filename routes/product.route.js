cc7dffc6528413634b9aac9947bc5de59ec4dae7
Adel Ahmed <adel15952@gmail.com>
2026-02-01 15:44:50 +0200

    feat: Add product creation route with image upload and admin authentication.

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createProduct } from "../controllers/product.controller.js";
 import { uploadMultipleImages } from "../middleware/upload.middleware.js";
+import { adminAuth } from "../middleware/roles.middleware.js";
 
 const router = express.Router();
 
-router.post("/", uploadMultipleImages, createProduct);
+router.post("/", adminAuth, uploadMultipleImages, createProduct);
 
 export default router;
