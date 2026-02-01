48d0f0534562b79cba8ba241812ede10f3e93439
Adel Ahmed <adel15952@gmail.com>
2026-02-01 23:20:21 +0200

    feat: add Mongoose Order model with schema definition, item details, total price calculation, and status management.

--- models/order.model.js
+++ models/order.model.js
@@ -42,7 +42,6 @@ const orderSchema = new mongoose.Schema(
     },
   },
   { timestamps: true },
-  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
 );
 
 orderSchema.plugin(toJSONPlugin);
