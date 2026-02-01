3affd2d7bdf0ac65c0c857362636c4361d30ea5d
Adel Ahmed <adel15952@gmail.com>
2026-02-01 23:08:34 +0200

    feat: Add Mongoose schema and model for orders, including order items and user references.

---
+++ models/order.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Quantity is required"],
+    min: [1, "Quantity must be at least 1"],
+    max: [100, "Quantity must be at most 100"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    totalPrice: {
+      type: Number,
+      required: [true, "Total price is required"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
