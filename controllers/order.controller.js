761a3c85bbf092bc4eb2846ab355722ecd6df621
Adel Ahmed <adel15952@gmail.com>
2026-02-01 23:35:24 +0200

    feat: implement initial order creation logic with item and quantity validation in `order.controller.js`.

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,6 @@
 import { successResponse, errorResponse } from "../helpers/response.js";
 import Order from "../models/order.model.js";
+import Product from "../models/product.model.js";
 
 export const createOrder = async (req, res) => {
   try {
@@ -9,6 +10,23 @@ export const createOrder = async (req, res) => {
     // validate order items
     if (!orderItems || orderItems.length === 0 || !Array.isArray(orderItems)) {
       return errorResponse(res, 400, "No order items provided");
+    }
+    for (const item of orderItems) {
+      if (!item.product || !item.quantity) {
+        return errorResponse(res, 400, "Invalid order items");
+      }
+
+      const product = await Product.findById(item.product);
+      if (!product) {
+        return errorResponse(res, 404, "Product not found");
+      }
+
+      if(typeof item.quantity !== "number" || item.quantity < 1){
+        return errorResponse(res, 400, "Invalid quantity");
+      }
+      if(!Number.isInteger(item.quantity)){
+        return errorResponse(res, 400, "Invalid quantity");
+      }
     }
     successResponse(res, 201, "Order created successfully", order);
   } catch (error) {
